36b0163d87b8da83ae421178728ff22baaafe458
Sokolovafm <danila.komarov.0300@mail.ru>
2022-12-09 21:26:16 +0300

    newadd

---
+++ UP1.1-1.3/DotaTrip/DotaTrip/Classes/DbConnect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DotaTrip.Classes
+{
+    class DbConnect
+    {
+        public static Models.TourAgencyMKEntities modeldb;
+    }
+}
